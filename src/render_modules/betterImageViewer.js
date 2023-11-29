8dae084f472abd0f15fcc385f86d16c807020501
xiyuesaves <xiyuesaves@gmail.com>
2023-11-29 19:07:11 +0800

    优化媒体预览快捷关闭功能

--- src/render_modules/betterImageViewer.js
+++ src/render_modules/betterImageViewer.js
@@ -6,38 +6,25 @@ import { options } from "./options.js";
 function betterImageViewer() {
   // 修复弹窗字体模糊
   document.body.classList.add("image-viewer");
-  // 针对图片的单击关闭图片
-  const appEl = document.querySelector("#app");
-  const option = { attributes: false, childList: true, subtree: true };
-  const callback = (mutationsList, observer) => {
-    const element = document.querySelector(".main-area__content");
-    const video = document.querySelector("embed");
-    if (element && options.imageViewer.quickClose) {
-      observer.disconnect();
-      let isMove = false;
-      element.addEventListener("mousedown", (event) => {
-        if (event.button === 0) {
-          isMove = false;
-        }
-      });
-      element.addEventListener("mousemove", (event) => {
-        if (event.button === 0) {
-          isMove = true;
-        }
-      });
-      element.addEventListener("mouseup", (event) => {
-        let rightMenu = document.querySelector("#qContextMenu");
-        if (!isMove && event.button === 0 && !rightMenu) {
-          document.querySelector(`div[aria-label="关闭"]`).click();
-        }
-      });
-    } else if (video) {
-      // 判断打开的是视频
-      observer.disconnect();
+  let isMove = false;
+  document.addEventListener("mousedown", (event) => {
+    if (event.button === 0) {
+      isMove = false;
     }
-  };
-  const observer = new MutationObserver(callback);
-  observer.observe(appEl, option);
+  });
+  document.addEventListener("mousemove", (event) => {
+    if (event.button === 0) {
+      isMove = true;
+    }
+  });
+  document.addEventListener("mouseup", (event) => {
+    const rightMenu = document.querySelector("#qContextMenu");
+    const video = document.querySelector("embed");
+    if (!isMove && event.button === 0 && !rightMenu && !video) {
+      console.log("关闭");
+      document.querySelector(`div[aria-label="关闭"]`).click();
+    }
+  });
 }
 
 export { betterImageViewer };
