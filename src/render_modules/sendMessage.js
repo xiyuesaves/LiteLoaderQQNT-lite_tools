7bf54fd8c62f9350042d1cf6f9ec1e58639a4309
xiyuesaves <xiyuesaves@gmail.com>
2023-11-30 16:40:31 +0800

    移除模块

--- src/render_modules/sendMessage.js
+++
@@ -1,8 +0,0 @@
-import { logs } from "./logs.js";
-const log = new logs("发送消息模块").log;
-const webContentsId = lite_tools.getWebContentsId();
-log("已获取到webContentsId", webContentsId);
-
-function sendMessage() {}
-
-export { sendMessage };
