3c2a492840adebd611392aaefbe0088c9bee1f7e
xiyuesaves <xiyuesaves@gmail.com>
2023-12-08 12:59:57 +0800

    如果分享中没有携带外部url，则不进行转换

--- src/main_modules/replaceArk.js
+++ src/main_modules/replaceArk.js
@@ -11,31 +11,36 @@ function getArkData(json) {
 
 // 卡片替换函数
 function replaceArk(json, msg_seq) {
-  return JSON.stringify({
-    app: "com.tencent.structmsg",
-    config: json.config,
-    desc: "新闻",
-    extra: { app_type: 1, appid: json.meta.detail_1.appid, msg_seq, uin: json.meta.detail_1.host.uin },
-    meta: {
-      news: {
-        action: "",
-        android_pkg_name: "",
-        app_type: 1,
-        appid: json.meta.detail_1.appid,
-        ctime: json.config.ctime,
-        desc: json.meta.detail_1.desc,
-        jumpUrl: json.meta.detail_1.qqdocurl.replace(/\\/g, ""),
-        preview: json.meta.detail_1.preview,
-        source_icon: json.meta.detail_1.icon,
-        source_url: "",
-        tag: getArkData(json),
-        title: getArkData(json),
-        uin: json.meta.detail_1.host.uin,
+  // 如果分享中没有携带外部url，则不进行转换
+  if (json.meta.detail_1.qqdocurl) {
+    return JSON.stringify({
+      app: "com.tencent.structmsg",
+      config: json.config,
+      desc: "新闻",
+      extra: { app_type: 1, appid: json.meta.detail_1.appid, msg_seq, uin: json.meta.detail_1.host.uin },
+      meta: {
+        news: {
+          action: "",
+          android_pkg_name: "",
+          app_type: 1,
+          appid: json.meta.detail_1.appid,
+          ctime: json.config.ctime,
+          desc: json.meta.detail_1.desc,
+          jumpUrl: json.meta.detail_1.qqdocurl.replace(/\\/g, ""),
+          preview: json.meta.detail_1.preview,
+          source_icon: json.meta.detail_1.icon,
+          source_url: "",
+          tag: getArkData(json),
+          title: getArkData(json),
+          uin: json.meta.detail_1.host.uin,
+        },
       },
-    },
-    prompt: `[分享]${getArkData(json)}`,
-    ver: "0.0.0.1",
-    view: "news",
-  });
+      prompt: `[分享]${getArkData(json)}`,
+      ver: "0.0.0.1",
+      view: "news",
+    });
+  } else {
+    return JSON.stringify(json);
+  }
 }
 exports.replaceArk = replaceArk;
