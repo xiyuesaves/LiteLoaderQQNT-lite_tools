0da6dcd9aff127a08606073f7b6f12c3c6b8e923
xiyuesaves <xiyuesaves@gmail.com>
2024-01-20 00:21:23 +0800

    修复引用模块方式错误

--- src/main_modules/localEmoticons.js
+++ src/main_modules/localEmoticons.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { debounce } = require("./debounce");
+const debounce = require("./debounce");
 
 let callbackFunc = new Set();
 let emoticonsList = [];
