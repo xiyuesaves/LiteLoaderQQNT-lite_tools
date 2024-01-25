bce374e09fd5cc3c248c6bc4715693f99868729d
xiyuesaves <xiyuesaves@gmail.com>
2024-01-26 01:22:23 +0800

    优化快速关闭图片逻辑，修复有概率不触发单机关闭功能的问题

--- src/render_modules/betterImageViewer.js
+++ src/render_modules/betterImageViewer.js
@@ -1,26 +1,30 @@
 import { options } from "./options.js";
-
+import { logs } from "./logs.js";
+const log = new logs("快速关闭图片").log;
 /**
  * 媒体预览增强
  */
 function betterImageViewer() {
+  log("模块加载");
   // 修复弹窗字体模糊
   document.body.classList.add("image-viewer");
-  let isMove = false;
+  let offset = 0;
   document.addEventListener("mousedown", (event) => {
-    if (event.button === 0) {
-      isMove = false;
+    if (event.buttons === 1) {
+      offset = 0;
+    } else {
+      offset = 3;
     }
   });
   document.addEventListener("mousemove", (event) => {
-    if (event.button === 0) {
-      isMove = true;
+    if (event.buttons === 1) {
+      offset += Math.abs(event.movementX) + Math.abs(event.movementY);
     }
   });
   document.addEventListener("mouseup", (event) => {
     const rightMenu = document.querySelector(".q-context-menu");
     const video = document.querySelector("embed");
-    if (!isMove && event.button === 0 && !rightMenu && !video && options.imageViewer.quickClose) {
+    if (offset < 2 && event.buttons === 0 && !rightMenu && !video && options.imageViewer.quickClose) {
       if (event.target.closest(".main-area__content")) {
         document.querySelector(`div[aria-label="关闭"]`).click();
       }
