311233d467866c80a13d7b9e571d8147e361469c
xiyuesaves <xiyuesaves@gmail.com>
2024-02-06 22:01:47 +0800

    调整转发消息界面注入方式

--- src/pages/forward.js
+++ src/pages/forward.js
@@ -1,5 +1,5 @@
 import { updateWallpaper } from "../render_modules/updateWallpaper.js";
-import { observerMessageList } from "../render_modules/observerMessageList.js";
+import { observerMessageList, processMessageElement } from "../render_modules/observerMessageList.js";
 // 右键菜单相关操作
 import { addEventqContextMenu } from "../render_modules/qContextMenu.js";
 import { Logs } from "../render_modules/logs.js";
@@ -8,14 +8,12 @@ const log = new Logs("转发界面");
 document.querySelector("#app").classList.add("forward");
 updateWallpaper();
 addEventqContextMenu();
-const observe = new MutationObserver(forward);
-observe.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
 
 function forward() {
   observerMessageList(".list .q-scroll-view", ".list .q-scroll-view > div", true);
+  processMessageElement();
+  setTimeout(processMessageElement, 500);
 }
+forward();
 
 log("模块加载完成");
--- src/render_modules/observerMessageList.js
+++ src/render_modules/observerMessageList.js
@@ -226,4 +226,4 @@ function processMessageElement() {
   }
 }
 
-export { observerMessageList };
+export { observerMessageList, processMessageElement };
