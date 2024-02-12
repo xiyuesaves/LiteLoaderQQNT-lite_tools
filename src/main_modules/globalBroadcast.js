95bd97f503d6b3d0b92d7e397d985f4058405692
xiyuesaves <xiyuesaves@gmail.com>
2024-02-13 00:35:17 +0800

    调整全局广播模块

--- src/main_modules/globalBroadcast.js
+++ src/main_modules/globalBroadcast.js
@@ -1,10 +1,10 @@
 const { BrowserWindow } = require("electron");
 // 向所有未销毁页面发送广播
-function globalBroadcast(channel, data) {
+function globalBroadcast(channel, ...data) {
   allWindows = BrowserWindow.getAllWindows();
   allWindows.forEach((window) => {
     if (!window.isDestroyed()) {
-      window.webContents.send(channel, data);
+      window.webContents.send(channel, ...data);
     }
   });
 }
