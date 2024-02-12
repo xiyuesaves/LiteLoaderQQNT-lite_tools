95ee45b761167c33feed38b45e8aef65bd761560
xiyuesaves <xiyuesaves@gmail.com>
2024-02-12 15:57:46 +0800

    删除注释

--- src/shell/win_getFonts.ps1
+++ src/shell/win_getFonts.ps1
@@ -1,4 +1,3 @@
 chcp 65001
 [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 (New-Object System.Drawing.Text.InstalledFontCollection).Families
-# Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
