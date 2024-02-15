176ae7cb5b9ed757fc1465dcf23d4b8da5eb2f6d
ghostflyby <ghostflyby+git@outlook.com>
2024-02-15 20:43:49 +0800

    replace chcp with powershell

--- src/main_modules/getFonts.js
+++ src/main_modules/getFonts.js
@@ -2,9 +2,7 @@ const { exec } = require("node:child_process");
 function winGetFonts() {
   return new Promise((res, rej) => {
     exec(
-      `if([System.Text.Encoding]::Default.WindowsCodePage -eq 65001){
-         chcp 65001;
-      }
+      `[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
       [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing");
       (New-Object System.Drawing.Text.InstalledFontCollection).Families;`,
       {
