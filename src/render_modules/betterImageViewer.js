921fa20eccb6621035f8970f43a1623512299159
xiyuesaves <xiyuesaves@gmail.com>
2024-03-26 10:56:47 +0800

    修复 #176 过大尺寸的图片无法启用 快速关闭 功能

--- src/render_modules/betterImageViewer.js
+++ src/render_modules/betterImageViewer.js
@@ -9,27 +9,39 @@ function betterImageViewer() {
   // 修复弹窗字体模糊
   document.body.classList.add("image-viewer");
   let offset = 0;
-  document.addEventListener("mousedown", (event) => {
-    if (event.buttons === 1) {
-      offset = 0;
-    } else {
-      offset = 3;
-    }
-  });
-  document.addEventListener("mousemove", (event) => {
-    if (event.buttons === 1) {
-      offset += Math.abs(event.movementX) + Math.abs(event.movementY);
-    }
-  });
-  document.addEventListener("mouseup", (event) => {
-    const rightMenu = document.querySelector(".q-context-menu");
-    const video = document.querySelector("embed");
-    if (offset < 2 && event.buttons === 0 && !rightMenu && !video && options.imageViewer.quickClose) {
-      if (event.target.closest(".main-area__content")) {
-        document.querySelector(`div[aria-label="关闭"]`).click();
+  document.addEventListener(
+    "pointerdown",
+    (event) => {
+      if (event.buttons === 1) {
+        offset = 0;
+      } else {
+        offset = 3;
       }
-    }
-  });
+    },
+    true,
+  );
+  document.addEventListener(
+    "pointermove",
+    (event) => {
+      if (event.buttons === 1) {
+        offset += Math.abs(event.movementX) + Math.abs(event.movementY);
+      }
+    },
+    true,
+  );
+  document.addEventListener(
+    "pointerup",
+    (event) => {
+      const rightMenu = document.querySelector(".q-context-menu");
+      const video = document.querySelector("embed");
+      if (offset < 2 && event.buttons === 0 && !rightMenu && !video && options.imageViewer.quickClose) {
+        if (event.target.closest(".main-area__content")) {
+          document.querySelector(`div[aria-label="关闭"]`).click();
+        }
+      }
+    },
+    true,
+  );
 }
 
 export { betterImageViewer };
