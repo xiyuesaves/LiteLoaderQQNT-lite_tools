b0b3cd9bcb75a9d1ade095db98704f970e8a8b39
xiyuesaves <xiyuesaves@gmail.com>
2024-04-05 22:03:43 +0800

    优化消息缓存模块

--- src/main_modules/LimitedMap.js
+++ src/main_modules/LimitedMap.js
@@ -5,16 +5,13 @@ class LimitedMap {
   constructor(limit) {
     this.limit = limit;
     this.map = new Map();
-    this.keys = [];
   }
   set(key, value) {
     // 如果当前map存储消息超过指定大小，则删除最后一条数据
     if (this.map.size >= this.limit) {
-      const oldestKey = this.keys.shift();
-      this.map.delete(oldestKey);
+      this.deleteOldData();
     }
     this.map.set(key, value);
-    this.keys.push(key);
   }
   get(key) {
     return this.map.get(key);
@@ -25,11 +22,13 @@ class LimitedMap {
   toArray() {
     return Array.from(this.map);
   }
+  deleteOldData() {
+    const array = Array.from(this.map);
+    const arrayLength = array.length;
+    // 每次移除10%
+    this.map = new Map(array.splice(0, arrayLength - arrayLength * 0.1));
+  }
   delete(key) {
-    const index = this.keys.indexOf(key);
-    if (index !== -1) {
-      this.keys.splice(index, 1);
-    }
     this.map.delete(key);
   }
 }
