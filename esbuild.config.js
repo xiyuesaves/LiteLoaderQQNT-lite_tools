aba78ecbe9fe6e599be3e1e961f208204c6ba15d
xiyuesaves <xiyuesaves@gmail.com>
2024-04-08 11:33:17 +0800

    调整编译脚本

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,29 @@
-const esbuildConig = () =>
-  require("esbuild").buildSync({
-    entryPoints: ["./src/main.js"],
-    bundle: true,
-    outfile: "./dist/main.js",
-    target: "node16",
-    platform: "node",
-    charset: "utf8",
-    external: ["electron", "sass"],
-  });
-
-esbuildConig();
+const { buildSync } = require("esbuild");
+// 主进程
+buildSync({
+  entryPoints: ["./src/main.js"],
+  bundle: true,
+  outfile: "./dist/main.js",
+  target: "node16",
+  platform: "node",
+  charset: "utf8",
+  external: ["electron", "sass"],
+});
+// debug页面
+buildSync({
+  entryPoints: ["./src/render_modules/debug.js"],
+  bundle: true,
+  outfile: "./dist/debug.js",
+  target: "es2020",
+  platform: "browser",
+  charset: "utf8",
+});
+// 渲染进程
+buildSync({
+  entryPoints: ["./src/renderer.js"],
+  bundle: true,
+  outfile: "./dist/render.js",
+  target: "es2020",
+  platform: "browser",
+  charset: "utf8",
+});
