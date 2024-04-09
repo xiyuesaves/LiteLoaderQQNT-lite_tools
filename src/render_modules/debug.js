f744e8d883eda9bf1d3eac804754e698badb5b98
xiyuesaves <xiyuesaves@gmail.com>
2024-04-09 15:15:12 +0800

    调整debug模块

--- src/render_modules/debug.js
+++ src/render_modules/debug.js
@@ -3,10 +3,12 @@ import superjson from "superjson";
 // 导出到window对象便于调试
 window.superjson = superjson;
 
+let loop = true;
+
 const port = location.port;
 console.log("通信端口", port);
 (async () => {
-  while (true) {
+  while (loop) {
     try {
       let log = superjson.parse(await (await fetch(`http://localhost:${port}/`)).text());
       if (log && log.length) {
@@ -18,7 +20,12 @@ console.log("通信端口", port);
         }
       }
     } catch (err) {
-      // console.log("=========已断开连接=========", err);
+      if (err.message === "Failed to fetch") {
+        loop = false;
+        console.log("=========已断开连接=========");
+      } else {
+        console.log("解码出错", err.message);
+      }
     }
   }
 })();
