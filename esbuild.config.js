f3d58fd69f678fd7f9031ac0141a9cf73b5af523
xiyuesaves <xiyuesaves@gmail.com>
2024-04-12 09:48:22 +0800

    优化打包脚本

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,4 +1,5 @@
 const { buildSync } = require("esbuild");
+let thisTime = new Date().getTime();
 // 主进程
 buildSync({
   entryPoints: ["./src/main.js"],
@@ -9,6 +10,8 @@ buildSync({
   charset: "utf8",
   external: ["electron", "sass"],
 });
+console.log(`主进程打包耗时：${(new Date().getTime() - thisTime) / 1000} s`);
+thisTime = new Date().getTime();
 // debug页面
 buildSync({
   entryPoints: ["./src/render_modules/debug.js"],
@@ -18,6 +21,7 @@ buildSync({
   platform: "browser",
   charset: "utf8",
 });
+console.log(`debug页面打包耗时：${(new Date().getTime() - thisTime) / 1000} s`);
 // 渲染进程 - 不需要被打包
 // buildSync({
 //   entryPoints: ["./src/renderer.js"],
