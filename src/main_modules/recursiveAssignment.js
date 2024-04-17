b18bfb9d32dcd454eb3dbe7ede705f5ee31381ae
xiyuesaves <xiyuesaves@gmail.com>
2024-04-17 11:26:24 +0800

    优化配置合并模块

--- src/main_modules/recursiveAssignment.js
+++ src/main_modules/recursiveAssignment.js
@@ -11,12 +11,18 @@ function recursiveAssignment(fileOptions, defaultOptions) {
   let obj = {};
   for (const key in defaultOptions) {
     if (Object.hasOwnProperty.call(defaultOptions, key)) {
+      // 如果是全局属性则直接覆盖配置文件
+      if (key === "global") {
+        obj[key] = defaultOptions[key];
+        continue;
+      }
       // 如果键值是对象则递归处理
       if (Object.prototype.toString.call(defaultOptions[key]) === "[object Object]") {
         obj[key] = recursiveAssignment(fileOptions[key], defaultOptions[key]);
+        continue;
       }
       // 判断两者同一个键的数据类型是否一致
-      else if (Object.prototype.toString.call(fileOptions[key]) === Object.prototype.toString.call(defaultOptions[key])) {
+      if (Object.prototype.toString.call(fileOptions[key]) === Object.prototype.toString.call(defaultOptions[key])) {
         obj[key] = fileOptions[key];
       } else {
         obj[key] = defaultOptions[key];
