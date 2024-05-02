88e78b907fbe7eea025d146ebe1716adcacb1414
xiyuesaves <xiyuesaves@gmail.com>
2024-05-03 02:49:01 +0800

    日志模块完成重构

--- src/main_modules/logs.js
+++ src/main_modules/logs.js
@@ -1,66 +1,41 @@
-const { shell } = require("electron");
-const fs = require("fs");
-const http = require("http");
-const net = require("net");
-const Opt = require("./option");
-const debounce = require("./debounce");
+import { shell } from "electron";
+import { createServer as createHttpServer } from "http";
+import { createServer as creatNetServer } from "net";
+import { readFileSync } from "fs";
 import superjson from "superjson";
-let options = Opt.value;
 
-/**
- * 日志类
- */
+import { config, onUpdateConfig } from "./config.js";
+
+let cacheLogs = [];
 class Logs {
+  constructor(logName) {
+    this.logName = logName;
+    return this.log.bind(this);
+  }
+  log(...args) {
+    console.log(`[${this.logName}]`, ...args);
+    cacheLogs.push([`[${this.logName}]`, ...args]);
+  }
+}
+class WebLog {
   constructor() {
-    this.logMsg = [];
-    this.res = [];
-    this.server = http.createServer(this.httpHandel.bind(this));
-    if (options.debug.showWeb) {
-      this.startLogServer();
-    }
-  }
-  createLog(logname) {
-    const name = logname;
-    const fn = (...args) => this.log(name, ...args);
-    return fn;
-  }
-  log = (moduleName, ...args) => {
-    if (options.debug.mainConsole) {
-      const logArr = [`\x1B[32m[LT]<${moduleName}> \x1B[0m`, ...args];
-      this.logMsg.push(logArr);
-      console.log(...logArr);
-    }
-  };
-  startLogServer() {
-    // 获取空闲端口号
-    const port = (() => {
-      const server = net.createServer();
-      server.listen(0);
-      const { port } = server.address();
-      server.close();
-      return port;
-    })();
-    if (!this.server.listening) {
-      this.server.listen(port, () => {
-        shell.openExternal(`http://localhost:${port}/debug`);
-      });
-    }
+    this.server = createHttpServer(this.httpHandel.bind(this));
   }
   httpHandel(req, res) {
     // 处理日志请求
     if (req.url === "/" && req.method === "GET") {
       // 读取日志文件内容
       res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8", "Access-Control-Allow-Origin": "*" });
-      const log = superjson.stringify(this.logMsg);
-      this.logMsg = [];
+      const log = superjson.stringify(cacheLogs);
+      cacheLogs = [];
       res.end(log);
     } else if (req.url === "/debug" && req.method === "GET") {
       res.writeHead(200, { "Content-Type": "text/html; charset=utf-8", "Access-Control-Allow-Origin": "*" });
-      const html = fs.readFileSync(`${LiteLoader.plugins.lite_tools.path.plugin}/src/html/debug.html`, { encoding: "utf-8" });
+      const html = readFileSync(`${LiteLoader.plugins.lite_tools.path.plugin}/src/html/debug.html`, { encoding: "utf-8" });
       res.end(html);
     } else if (req.url === "/debug.js" && req.method === "GET") {
       res.writeHead(200, { "Content-Type": "application/javascript; charset=utf-8", "Access-Control-Allow-Origin": "*" });
-      const js = fs.readFileSync(`${LiteLoader.plugins.lite_tools.path.plugin}/dist/debug.js`, { encoding: "utf-8" });
+      const js = readFileSync(`${LiteLoader.plugins.lite_tools.path.plugin}/dist/debug.js`, { encoding: "utf-8" });
       res.end(js);
     } else {
       // 处理其他请求
@@ -68,7 +43,21 @@ class Logs {
       res.end("Not Found");
     }
   }
-  stopLogServer() {
+  start() {
+    if (!this.server.listening) {
+      const port = (() => {
+        const server = creatNetServer();
+        server.listen(0);
+        const { port } = server.address();
+        server.close();
+        return port;
+      })();
+      this.server.listen(port, () => {
+        shell.openExternal(`http://localhost:${port}/debug`);
+      });
+    }
+  }
+  stop() {
     if (this.server.listening) {
       this.server.closeAllConnections();
       this.server.closeIdleConnections();
@@ -76,21 +65,17 @@ class Logs {
     }
   }
 }
+const webLog = new WebLog();
 
-const logs = new Logs();
-
-const debounceUpdateOptions = debounce((newOptions) => {
-  options = newOptions;
-  if (options.debug.showWeb) {
-    logs.startLogServer();
+onUpdateConfig(() => {
+  if (config.debug.showWeb) {
+    webLog.start();
   } else {
-    logs.stopLogServer();
+    webLog.stop();
   }
-  if (!options.debug.mainConsole) {
-    logs.logMsg = [];
+  if (!config.debug.mainConsole) {
+    cacheLogs = [];
   }
-}, 10);
+});
 
-Opt.on("update", debounceUpdateOptions);
-
-module.exports = logs.createLog.bind(logs);
+export { Logs, webLog };
