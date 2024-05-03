e05fb6a8bc0da00bef8da8c17b8446cc78875472
xiyuesaves <xiyuesaves@gmail.com>
2024-05-03 14:00:40 +0800

    重构广播模块

--- src/main_modules/globalBroadcast.js
+++ src/main_modules/globalBroadcast.js
@@ -1,15 +1,15 @@
-const { BrowserWindow } = require("electron");
+import { BrowserWindow } from "electron";
 /**
  * 向所有未销毁页面发送广播
  * @param {String} channel 事件名称
  * @param  {...any} data 发送数据
  */
 function globalBroadcast(channel, ...data) {
-  allWindows = BrowserWindow.getAllWindows();
+  const allWindows = BrowserWindow.getAllWindows();
   allWindows.forEach((window) => {
     if (!window.isDestroyed()) {
       window.webContents.send(channel, ...data);
     }
   });
 }
-module.exports = globalBroadcast;
+export { globalBroadcast };
