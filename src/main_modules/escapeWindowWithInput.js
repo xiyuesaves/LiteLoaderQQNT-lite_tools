e7003d5506c0636024f7a925e00b447930b149ae
囧囧JOJO <41473909@qq.com>
2024-05-14 13:56:17 +0800

    优化：ESC隐藏窗口时避免多次触发 增加ESC隐藏窗口的异常捕获

--- src/main_modules/escapeWindowWithInput.js
+++ src/main_modules/escapeWindowWithInput.js
@@ -1,4 +1,6 @@
 import { config } from "./config.js";
+import { Logs } from "./logs.js";
+const log = new Logs("ESC 隐藏窗口");
 
 /**
  * 忽略输入框内是否有内容，ESC强制关闭窗口。
@@ -10,8 +12,13 @@ export function escapeWindowWithInput(window) {
   window.webContents.on("before-input-event", async (event, input) => {
     // 阻止按下 ESC 键时关闭窗口
     if (input.key == "Escape") {
-        if (config.escapeWindowWithInput && !config.preventEscape) {
-          window.hide();
+        try {
+            if (config.escapeWindowWithInput && !config.preventEscape) {
+                window.hide();
+                event.preventDefault();
+            }
+        } catch (err) {
+            log("出现错误", err, err?.stack);
         }
     }
   });
