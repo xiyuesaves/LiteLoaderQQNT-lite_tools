ed55cff6e23b625b43e36f1ff55b8e03670259b9
囧囧JOJO <41473909@qq.com>
2024-05-15 15:06:03 +0800

    优化：ESC松开后才会触发事件，避免重复处理

--- src/main_modules/escapeWindowWithInput.js
+++ src/main_modules/escapeWindowWithInput.js
@@ -9,13 +9,12 @@ const log = new Logs("ESC 隐藏窗口");
  * @return {void} 此函数不返回任何内容。
  */
 export function escapeWindowWithInput(window) {
-  window.webContents.on("before-input-event", async (event, input) => {
+  window.webContents.on("keyup", async (event, input) => {
     // 阻止按下 ESC 键时关闭窗口
     if (input.key == "Escape") {
         try {
             if (config.escapeWindowWithInput && !config.preventEscape) {
                 window.hide();
-                event.preventDefault();
             }
         } catch (err) {
             log("出现错误", err, err?.stack);
