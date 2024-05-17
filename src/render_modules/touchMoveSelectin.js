5a9e732e1884e558e067f5f95747f7751df35ca1
xiyuesaves <xiyuesaves@gmail.com>
2024-05-17 11:40:08 +0800

    修复 阻止拖拽多选消息 功能启用时会影响拖拽滚动条的问题

--- src/render_modules/touchMoveSelectin.js
+++ src/render_modules/touchMoveSelectin.js
@@ -1,24 +1,48 @@
 import { options } from "./options.js";
 import { Logs } from "./logs.js";
 const log = new Logs("阻止滑动选择");
+/**
+ * 是否已捕获目标
+ */
 let listenTarget = false;
+
+/**
+ * 是否拦截事件
+ */
+let interception = false;
+
 /**
  * 阻止拖拽多选消息
  * @param {String} className 禁止拖拽类名
  */
 function touchMoveSelectin(className) {
   log("模块已加载", options.message.disabledSlideMultipleSelection);
-  let interception;
-  document.querySelector("#app").addEventListener("mousedown", (event) => {
+  document.querySelector("#app").addEventListener("pointerdown", (event) => {
     if (options.message.disabledSlideMultipleSelection && (event.buttons === 1 || event.buttons === 4)) {
-      interception = !event.target.closest(".message-content__wrapper") && event.target.closest(`.${className}`);
+      interception = !!(
+        !event.target.closest(".message-content__wrapper") &&
+        event.target.closest(`.${className}`) &&
+        !event.target.closest(".v-scrollbar-track")
+      );
+    } else {
+      interception = false;
+    }
+    log("更新状态", interception);
+  });
+  document.querySelector("#app").addEventListener("pointerup", (event) => {
+    if (options.message.disabledSlideMultipleSelection) {
+      if (event.buttons === 0) {
+        interception = false;
+      }
+      log("更新状态", interception);
     }
   });
 
   document.querySelector("#app").addEventListener("mousemove", (event) => {
+    // 判断是否已捕获目标
     if (!listenTarget && document.querySelector(`.${className}`)) {
       log("已捕获目标元素");
-      document.querySelector(`.${className}`).addEventListener("mousedown", (event) => {
+      document.querySelector(`.${className}`).addEventListener("pointerdown", (event) => {
         if (options.message.disabledSlideMultipleSelection && (event.buttons === 1 || event.buttons === 4)) {
           document.querySelector(".q-context-menu")?.remove();
         }
