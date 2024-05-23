f32d11863c52b668aace3f04034b812b5ad99456
xiyuesaves <xiyuesaves@gmail.com>
2024-05-23 17:26:41 +0800

    调整逻辑实现

--- src/main_modules/proxyOn.js
+++ src/main_modules/proxyOn.js
@@ -1,18 +1,6 @@
-import { config, onUpdateConfig } from "./config.js";
+import { config } from "./config.js";
 import { Logs } from "./logs.js";
 const log = new Logs("proxyOn");
-
-/**
- * 窗口 resized 函数
- */
-let resizeFunc;
-
-onUpdateConfig(() => {
-  if (resizeFunc) {
-    log("配置更新调用一次");
-    resizeFunc();
-  }
-});
 
 /**
  * 代理window on对象
@@ -25,25 +13,22 @@ function proxyOn(window) {
       apply(target, thisArg, args) {
         try {
           if (args[0] === "resized") {
-            resizeFunc = args[1];
+            const resizeFunc = args[1];
             args[1] = (...args) => {
               if (!config.message.unlockMainMinSize) {
-                log("通过-pass", args);
                 resizeFunc(...args);
-              } else {
-                log("拦截");
               }
             };
           }
           return target.apply(thisArg, args);
         } catch (err) {
-          log("内部出现错误", err);
+          log("函数调用出现错误", err);
         }
       },
     });
     window.on = proxtOn;
   } catch (err) {
-    log("出现错误", err);
+    log("代理on方法出现错误", err);
   }
 }
 
