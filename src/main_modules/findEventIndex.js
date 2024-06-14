f7fd135f4c0842ce03d1fb131b57c439e2144ac1
xiyuesaves <xiyuesaves@gmail.com>
2024-06-14 12:25:42 +0800

    优化事件查找函数

--- src/main_modules/findEventIndex.js
+++ src/main_modules/findEventIndex.js
@@ -1,9 +1,18 @@
 /**
  * 判断事件名称
  * @param {Array} args 目标数组
- * @param {String} eventName 目标字符串
- * @returns {Boolean}
+ * @param {String|String[]} eventName 目标字符串,目标字符串数组
+ * @returns {Number} 目标字符串在数组中的索引,没有找到返回-1
  */
 export function findEventIndex(args, eventName) {
-  return args?.[2] ? (Array.isArray(args[2]) ? args[2].findIndex((item) => item.cmdName === eventName) : -1) : -1;
+  if (!args?.[2] || !Array.isArray(args[2])) {
+    return -1;
+  }
+  if (Array.isArray(eventName)) {
+    return args[2].findIndex((item) => eventName.some((event) => item.cmdName === event));
+  } else if (typeof eventName === "string") {
+    return args[2].findIndex((item) => item.cmdName === eventName);
+  } else {
+    return -1;
+  }
 }
