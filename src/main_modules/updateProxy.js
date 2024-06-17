4bb99a90c51762f24adf44a8ea33ea0aa8f06a61
xiyuesaves <xiyuesaves@gmail.com>
2024-06-17 17:16:09 +0800

    调整请求相关逻辑

--- src/main_modules/updateProxy.js
+++ src/main_modules/updateProxy.js
@@ -1,4 +1,5 @@
-import { setGlobalDispatcher, ProxyAgent } from "undici";
+// import { ipcMain } from "electron";
+import { fetch, setGlobalDispatcher, ProxyAgent } from "undici";
 import { onUpdateConfig } from "./config.js";
 import { Logs } from "./logs.js";
 const log = new Logs("updateProxy");
@@ -9,6 +10,17 @@ onUpdateConfig((config) => {
       setProxyUrl = config.proxy.url;
       setGlobalDispatcher(new ProxyAgent(config.proxy.url));
       log("更新代理地址成功", setProxyUrl);
+      fetch("http://www.google.com/generate_204")
+        .then((res) => {
+          if (res.status === 204) {
+            log("代理地址可用", setProxyUrl);
+          } else {
+            log("代理地址不可用-1", res.status);
+          }
+        })
+        .catch(() => {
+          log("代理地址不可用-2", setProxyUrl);
+        });
     }
   } catch (err) {
     log("代理地址有误", err);
