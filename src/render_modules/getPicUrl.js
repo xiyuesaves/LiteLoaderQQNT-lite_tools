6e65c7020fa8183bf9e76bc612a8e869c8f58c32
xiyuesaves <xiyuesaves@gmail.com>
2024-06-18 09:09:16 +0800

    修复 右键菜单>图片搜索 功能的url拼接逻辑错误

--- src/render_modules/getPicUrl.js
+++ src/render_modules/getPicUrl.js
@@ -1,14 +1,15 @@
 import { options } from "./options.js";
 export async function getPicUrl(picData, chatType) {
-  if (picData.originImageUrl) {
+  let picURL = "";
+  if (picData.original) {
     if (picData.originImageUrl.includes("&rkey=")) {
       return `${options.global.IMAGE_HTTP_HOST_NT}${picData.originImageUrl}`;
     } else {
       const rkey = await lite_tools.getRkey(chatType);
-      console.log("返回rkey",rkey);
-      return `${options.global.IMAGE_HTTP_HOST_NT}${picData.originImageUrl}${rkey}`;
+      picURL = `${options.global.IMAGE_HTTP_HOST_NT}${picData.originImageUrl}${rkey}`;
     }
   } else {
-    return `${options.global.IMAGE_HTTP_HOST}/gchatpic_new/0/0-0-${picData.md5HexStr.toUpperCase()}/0`;
+    picURL = `${options.global.IMAGE_HTTP_HOST}${picData.originImageUrl}`;
   }
+  return picURL;
 }
