b209e4fea6595019aa72e32730da27629350d2b5
xiyuesaves <xiyuesaves@gmail.com>
2024-06-18 09:08:00 +0800

    修复 #263 getRkey 模块无法正常工作的问题

--- src/main_modules/getRkey.js
+++ src/main_modules/getRkey.js
@@ -4,16 +4,15 @@ const log = new Logs("getRkey");
 
 let rkey;
 export async function getRkey(chatType) {
-  log("请求rkey", config.rkeyAPI, chatType);
+  log("请求rkey", chatType);
   if (!config.rkeyAPI || !chatType) {
     log("没有配置rkey或没有传入type");
     return config.global.rkey;
   }
-  if (!rkey || rkey.expired_time ? rkey.expired_time < Date.now() / 1000 : true) {
+  if (!rkey || (rkey.expired_time ? rkey.expired_time < Date.now() / 1000 : true)) {
     rkey = await fetchRkey();
-    if (rkey) {
-      log("成功更新rkey", rkey);
-    } else {
+    if (!rkey) {
+      log("获取rkey失败");
       return config.global.rkey;
     }
   }
@@ -24,9 +23,18 @@ export async function getRkey(chatType) {
 
 async function fetchRkey() {
   try {
-    return await fetch(config.rkeyAPI).then((res) => res.json());
+    log("正在更新rkey", config.rkeyAPI);
+    const newRkey = await fetch(config.rkeyAPI).then((res) => res.json());
+    if (typeof newRkey.group_rkey === "string" && typeof newRkey.private_rkey === "string") {
+      rkey = newRkey;
+      log("成功更新rkey", rkey);
+    } else {
+      log("数据返回格式错误", newRkey);
+      return undefined;
+    }
+    return rkey;
   } catch (err) {
     log("获取rkey失败", err);
-    return null;
+    return undefined;
   }
 }
