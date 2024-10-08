1b4d7096134e2d1cce677fde03fe3016b984ac8b
xiyuesaves <xiyuesaves@gmail.com>
2024-10-08 12:03:57 +0800

    feat: 新增 阻止撤回>重定向图片储存路径, 优化阻止撤回图片下载逻辑

--- src/main_modules/processPic.js
+++ src/main_modules/processPic.js
@@ -1,4 +1,5 @@
-import { dirname } from "path";
+import { dirname, join, basename } from "path";
+import { loadConfigPath } from "./config.js";
 import { existsSync, mkdirSync, writeFileSync, statSync } from "fs";
 import { downloadPic } from "./downloadPic.js";
 import { config } from "./config.js";
@@ -19,10 +20,15 @@ async function getPicUrl(picData, chatType) {
   } else {
     picURL = `${config.global.IMAGE_HTTP_HOST}${picData.originImageUrl}`;
   }
-  log("获取图片链接", picURL);
   return picURL;
 }
 
+/**
+ * 获取图片链接数组
+ * @param {Object} picData 图片对象
+ * @param {String} chatType 消息类型
+ * @returns String[]
+ */
 async function getPicArr(picData, chatType) {
   const rawUrl = await getPicUrl(picData, chatType);
   return [0, 198, 720].map((el) => {
@@ -46,35 +52,60 @@ async function processPic(msgItem) {
         log("该消息含有图片", el);
         const pic = el.picElement;
         const chatType = msgItem.chatType === 2 ? "group_rkey" : "private_rkey";
-        const picUrls = await getPicArr(pic, chatType);
+        // 另存为图片到插件配置目录
+        if (config.preventMessageRecall.redirectPicPath) {
+          pic.sourcePath = join(loadConfigPath, `preventMessageRecall/Ori/${basename(pic.sourcePath)}`);
+        }
+        // 图片url数组
+        let picUrls = null;
         if (requireDownload(pic.sourcePath)) {
+          picUrls = await getPicArr(pic, chatType);
           try {
-            log("下载原图", picUrls[0]);
+            log("下载原图");
             const body = await downloadPic(picUrls[0]);
             mkdirSync(dirname(pic.sourcePath), { recursive: true });
             writeFileSync(pic.sourcePath, body);
           } catch (err) {
             log("原图下载失败", picUrls[0], err?.message, err?.stack);
           }
-        } else {
-          log("原图已存在", pic.sourcePath);
         }
         if (pic?.thumbPath) {
+          if (picUrls) {
+            picUrls = await getPicArr(pic, chatType);
+          }
           pic.thumbPath = new Map([
             [0, pic.sourcePath],
-            [198, pic.sourcePath],
-            [720, pic.sourcePath],
+            [198, pic.sourcePath.replace("Ori", "Thumb").replace(pic.md5HexStr, pic.md5HexStr + "_198")],
+            [720, pic.sourcePath.replace("Ori", "Thumb").replace(pic.md5HexStr, pic.md5HexStr + "_720")],
           ]);
+          const thumbPics = Array.from(pic.thumbPath);
+          for (let i = 0; i < thumbPics.length; i++) {
+            const thumbPicPath = thumbPics[i][1];
+            if (requireDownload(thumbPicPath)) {
+              try {
+                log("下载缩略图", thumbPicPath, picUrls[i]);
+                const body = await downloadPic(picUrls[i]);
+                mkdirSync(dirname(thumbPicPath), { recursive: true });
+                writeFileSync(thumbPicPath, body);
+              } catch (err) {
+                log("缩略图下载失败", picUrls[i], err?.message, err?.stack);
+              }
+            }
+          }
         }
       }
     }
   }
 }
 
+/**
+ * 判断是否需要下载图片
+ * @param {String} path 图片本地路径
+ * @returns {Boolean}
+ */
 function requireDownload(path) {
   if (existsSync(path)) {
     const stats = statSync(path);
-    log("图片尺寸", stats.size, stats.size >= 100);
     return stats.size < 100;
   }
   return true;
